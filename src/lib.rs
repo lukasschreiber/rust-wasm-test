18edd8236708f420bda326a44583b96219675a12
Lukas Schreiber <info@lukasschreiber.com>
2023-03-21 22:52:00 +0100

    use other example

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,7 @@ pub fn test3_d() {
     utils::set_panic_hook();
     
     let window = Window::new(WindowSettings {
-        title: "Shapes!".to_string(),
+        title: "Instanced Shapes!".to_string(),
         max_size: Some((1280, 720)),
         ..Default::default()
     })
@@ -39,48 +39,130 @@ pub fn test3_d() {
 
     let mut camera = Camera::new_perspective(
         window.viewport(),
-        vec3(5.0, 2.0, 2.5),
-        vec3(0.0, 0.0, -0.5),
-        vec3(0.0, 1.0, 0.0),
+        vec3(60.00, 50.0, 60.0), // camera position
+        vec3(0.0, 0.0, 0.0),     // camera target
+        vec3(0.0, 1.0, 0.0),     // camera up
         degrees(45.0),
         0.1,
         1000.0,
     );
-    let mut control = OrbitControl::new(*camera.target(), 1.0, 100.0);
+    let mut control = OrbitControl::new(vec3(0.0, 0.0, 0.0), 1.0, 1000.0);
 
-    let mut sphere = Gm::new(
-        Mesh::new(&context, &CpuMesh::sphere(16)),
-        PhysicalMaterial::new_transparent(
+    let light0 = DirectionalLight::new(&context, 1.0, Color::WHITE, &vec3(0.0, -0.5, -0.5));
+    let light1 = DirectionalLight::new(&context, 1.0, Color::WHITE, &vec3(0.0, 0.5, 0.5));
+
+    // Container for non instanced meshes.
+    let mut non_instanced_meshes = Vec::new();
+
+    // Instanced mesh object, initialise with empty instances.
+    let mut instanced_mesh = Gm::new(
+        InstancedMesh::new(&context, &Instances::default(), &CpuMesh::cube()),
+        PhysicalMaterial::new(
             &context,
             &CpuMaterial {
                 albedo: Color {
-                    r: 255,
-                    g: 0,
-                    b: 0,
-                    a: 200,
+                    r: 128,
+                    g: 128,
+                    b: 128,
+                    a: 255,
                 },
                 ..Default::default()
             },
         ),
     );
-    sphere.set_transformation(Mat4::from_translation(vec3(0.0, 0.0, 0.0)) * Mat4::from_scale(0.2));
-   
-    let light0 = DirectionalLight::new(&context, 1.0, Color::WHITE, &vec3(0.0, -0.5, -0.5));
-    let light1 = DirectionalLight::new(&context, 1.0, Color::WHITE, &vec3(0.0, 0.5, 0.5));
+    instanced_mesh.set_animation(|time| Mat4::from_angle_x(Rad(time)));
+
+    // Initial properties of the example, 2 cubes per side and non instanced.
+    let side_count = 4;
+    let is_instanced = true;
 
     window.render_loop(move |mut frame_input| {
-        camera.set_viewport(frame_input.viewport);
+        // Gui panel to control the number of cubes and whether or not instancing is turned on.
+        let panel_width = 0.0;
+        let viewport = Viewport {
+            x: (panel_width * frame_input.device_pixel_ratio) as i32,
+            y: 0,
+            width: frame_input.viewport.width
+                - (panel_width * frame_input.device_pixel_ratio) as u32,
+            height: frame_input.viewport.height,
+        };
+        camera.set_viewport(viewport);
+
+        // Camera control must be after the gui update.
         control.handle_events(&mut camera, &mut frame_input.events);
+
+        // Ensure we have the correct number of cubes, does no work if already correctly sized.
+        let count = side_count * side_count * side_count;
+        if non_instanced_meshes.len() != count {
+            non_instanced_meshes.clear();
+            for i in 0..count {
+                let mut gm = Gm::new(
+                    Mesh::new(&context, &CpuMesh::cube()),
+                    PhysicalMaterial::new(
+                        &context,
+                        &CpuMaterial {
+                            albedo: Color {
+                                r: 128,
+                                g: 128,
+                                b: 128,
+                                a: 255,
+                            },
+                            ..Default::default()
+                        },
+                    ),
+                );
+                let x = (i % side_count) as f32;
+                let y = ((i as f32 / side_count as f32).floor() as usize % side_count) as f32;
+                let z = (i as f32 / side_count.pow(2) as f32).floor();
+                gm.set_transformation(Mat4::from_translation(
+                    3.0 * vec3(x, y, z) - 1.5 * (side_count as f32) * vec3(1.0, 1.0, 1.0),
+                ));
+                gm.set_animation(|time| Mat4::from_angle_x(Rad(time)));
+                non_instanced_meshes.push(gm);
+            }
+        }
+
+        if instanced_mesh.instance_count() != count as u32 {
+            instanced_mesh.set_instances(&Instances {
+                transformations: (0..count)
+                    .map(|i| {
+                        let x = (i % side_count) as f32;
+                        let y =
+                            ((i as f32 / side_count as f32).floor() as usize % side_count) as f32;
+                        let z = (i as f32 / side_count.pow(2) as f32).floor();
+                        Mat4::from_translation(
+                            3.0 * vec3(x, y, z) - 1.5 * (side_count as f32) * vec3(1.0, 1.0, 1.0),
+                        )
+                    })
+                    .collect(),
+                ..Default::default()
+            });
+        }
+
+        // Always update the transforms for both the normal cubes as well as the instanced versions.
+        // This shows that the difference in frame rate is not because of updating the transforms
+        // and shows that the performance difference is not related to how we update the cubes.
+        let time = (frame_input.accumulated_time * 0.001) as f32;
+        instanced_mesh.animate(time);
+        non_instanced_meshes
+            .iter_mut()
+            .for_each(|m| m.animate(time));
+
+        // Then, based on whether or not we render the instanced cubes, collect the renderable
+        // objects.
+        let render_objects: Vec<&dyn Object> = if is_instanced {
+            instanced_mesh.into_iter().collect()
+        } else {
+            non_instanced_meshes
+                .iter()
+                .map(|x| x as &dyn Object)
+                .collect()
+        };
 
         frame_input
             .screen()
             .clear(ClearState::color_and_depth(0.8, 0.8, 0.8, 1.0, 1.0))
-            .render(
-                &camera,
-                sphere
-                    .into_iter(),
-                &[&light0, &light1],
-            );
+            .render(&camera, render_objects, &[&light0, &light1]);
 
         FrameOutput::default()
     });
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,13 @@
-import { useEffect } from "react"
+import { useState } from "react"
 import * as wasm from "wasm-example"
 
 function App() {
+  const [showCanvas, setShowCanvas] = useState(false)
+
   return (
     <div className="App">
-      <button onClick={() => wasm.test3_d()}>Test</button>
-      <canvas></canvas>
-      {wasm.funcy_string()}
+      {!showCanvas && <button onClick={() => { setShowCanvas(true); wasm.test3_d() }}>Show Rendering</button>}
+      <canvas style={{ display: (showCanvas ? "block" : "none"), width: "100%", height: "100%" }}></canvas>
     </div>
   )
 }
